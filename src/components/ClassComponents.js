9ec6dbf007b9dd9249ae5b52e6cc5005d73a5c9e
Googly-Boogly <mainalextaylor@gmail.com>
2022-05-09 14:50:43 -0500

    Add files via upload

---
+++ src/components/ClassComponents.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+    
+    
+class SomeClassComponent extends Component {
+    render() {
+        return (
+        <div>
+            <h1>{this.props.lastName}, {this.props.firstName}</h1>
+            <p>{this.props.age}</p>
+            <p>{this.props.hairColor}</p>
+        </div>
+        )
+        ;
+    }
+}
+    
+export default SomeClassComponent;
